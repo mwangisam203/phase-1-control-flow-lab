13ded0369735e17797e31c9866625d4d82f687b8
mwangisam203 <mwangisam203@gmail.com>
2023-09-23 22:11:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js
+
+// Function to determine the scuber greeting based on the distance in feet
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check the city and return a message
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to switch on charm based on the tip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+
